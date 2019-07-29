bae5b38feffb4cf5327de39780c6f1ee60204586
Yuchen Wang <wyc8094@gmail.com>
2019-07-29 18:47:04 +0800

    init_commit

---
+++ app/src/main/java/com/turtle/yucwang/turtle/TurtleActivity.kt
@@ -0,0 +1,12 @@
+package com.turtle.yucwang.turtle
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TurtleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_turtle)
+    }
+}
